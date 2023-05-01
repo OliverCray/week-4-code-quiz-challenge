2691079a5e49d77c2bc8f2e476b5e704a2e9e784
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-05-01 19:00:27 +0100

    Created onQuizStart function

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,8 +3,8 @@ var startQuiz = document.querySelector("#startQuiz")
 
 // Grab ids of each section and assign to respective variables
 var welcome = document.querySelector("#welcome")
-var welcome = document.querySelector("#quiz")
-var welcome = document.querySelector("#results")
+var quiz = document.querySelector("#quiz")
+var results = document.querySelector("#results")
 
 var choices = document.querySelector("#choices")
 var response = document.querySelector("#response")
@@ -17,3 +17,32 @@ var timeRemaining = 0
 var score = 0
 var questionNum = 0
 var countdown
+
+function onQuizStart() {
+    // Set timer to 75 seconds
+    timeRemaining = 75
+
+    // Start at first question
+    questionNum = 0
+
+    // Reset score
+    score = 0
+
+    // Start Timer
+    countdown = setInterval(function () {
+        if (timeRemaining > 0) {
+            timer.textContent = timeRemaining + " seconds remaining"
+        } else {
+            stopQuiz()
+        }
+        timeRemaining--
+    }, 1000)
+
+    welcome.style.display = 'none'
+    quiz.style.dispaly = 'flex'
+    results.style.display = 'none'
+
+    displayQuestion()
+}
+
+startQuiz.addEventListener("click", onQuizStart)
