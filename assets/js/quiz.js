660e45d5ef27703da4c646431c4fcf1f09b37596
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-05-01 18:50:16 +0100

    Set up global variables

---
+++ assets/js/quiz.js
@@ -0,0 +1,19 @@
+// Grab id of Start Quiz button and assign to a variable
+var startQuiz = document.querySelector("#startQuiz")
+
+// Grab ids of each section and assign to respective variables
+var welcome = document.querySelector("#welcome")
+var welcome = document.querySelector("#quiz")
+var welcome = document.querySelector("#results")
+
+var choices = document.querySelector("#choices")
+var response = document.querySelector("#response")
+
+var timer = document.querySelector("#timer")
+
+var summary = document.querySelector("#summary")
+
+var timeRemaining = 0
+var score = 0
+var questionNum = 0
+var countdown
