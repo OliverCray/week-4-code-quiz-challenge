42e3c156a52982abf0df5676b9989ba6118ef7b1
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-05-06 18:59:55 +0100

    Gave name and score separate classes, updated CSS

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -5,16 +5,24 @@ var highScores = JSON.parse(localStorage.getItem("highScores")) || []
 
 console.log(highScores)
 
-var result = document.createElement("div")
-result.classList.add('result')
+var resultName = document.createElement("div")
+resultName.classList.add('resultName')
 
-result.innerHTML = highScores.map(score => {
-    return `<div class="highScore">${score.name}</div>
-            <div class="highScore">${score.score}</div>`
+resultName.innerHTML = highScores.map(score => {
+    return `<div class="highScore">${score.name}</div>`
   })
   .join('')
   
-scoresheet.appendChild(result)
+var resultScore = document.createElement("div")
+resultScore.classList.add('resultScore')
+  
+resultScore.innerHTML = highScores.map(score => {
+    return `<div class="highScore">${score.score}</div>`
+    })
+    .join('')
+    
+scoresheet.appendChild(resultName)
+scoresheet.appendChild(resultScore)
 
 // Clears local storage and reloads the page
 function clearHighScores() {
