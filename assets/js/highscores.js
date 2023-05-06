9e69175de00d9b126a2405487fb01165561c4630
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-05-06 19:40:22 +0100

    Added comments

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,26 +1,30 @@
 var scoresheet = document.querySelector("#scoresheet")
 var clearScore = document.querySelector("#clear")
 
+// Get highscores out of local storage
 var highScores = JSON.parse(localStorage.getItem("highScores")) || []
 
 console.log(highScores)
 
+// Create elements for the areas that will hold the name and score
 var resultName = document.createElement("div")
 resultName.classList.add('resultName')
 
+var resultScore = document.createElement("div")
+resultScore.classList.add('resultScore')
+
+// Add html for displaying the name and score
 resultName.innerHTML = highScores.map(score => {
     return `<div class="highScore">${score.name}</div>`
   })
   .join('')
   
-var resultScore = document.createElement("div")
-resultScore.classList.add('resultScore')
-  
 resultScore.innerHTML = highScores.map(score => {
     return `<div class="highScore">${score.score}</div>`
     })
     .join('')
-    
+
+// Append the name and score information so that it is displayed on the high scores page
 scoresheet.appendChild(resultName)
 scoresheet.appendChild(resultScore)
 
@@ -30,4 +34,5 @@ function clearHighScores() {
     location.reload()
 }
 
+// Clear local storage when the Clear High Scores button is clicked
 clearScore.addEventListener("click", clearHighScores)
